178dd6e10648ac5733900b2ed8586ebb13631b27
Freeman <hsuhsiaohsuan@gmail.com>
2015-10-16 08:35:28 +0800

    [2015/10/16]     1. temp.

--- app/src/main/java/idv/hsu/gcmtest/MainActivity.java
+++ app/src/main/java/idv/hsu/gcmtest/MainActivity.java
@@ -1,13 +1,42 @@
 package idv.hsu.gcmtest;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String PROJECT_NUMBER = "electric-signal-109919";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void registerInBackground(final Context context) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                Log.d(TAG, "Start GCM regist");
+                try {
+                    GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+                    if (gcm != null) {
+                        String regId = gcm.register(PROJECT_NUMBER);
+                        Log.d(TAG, "regist over, regid:" + regId);
+                    }
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+                return null;
+            }
+        }.execute(null, null, null);
     }
 }
